08e38fa4e1cb34a6bc1ca5afe10705b8bbfc4178
aa_lozovoy <aalozzzovoy@gmail.com>
2025-03-10 23:17:50 +0300

    123

---
+++ tests/test_converter.py
@@ -0,0 +1,14 @@
+import unittest
+from src.converter import extract_audio_from_video, transcribe_audio
+
+class TestConverter(unittest.TestCase):
+    def test_audio_extraction(self):
+        # Тест извлечения аудио
+        pass
+
+    def test_transcription(self):
+        # Тест транскрибации
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
